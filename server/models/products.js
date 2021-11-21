c77d213c312fb6883ba5c2d1d428292b487d0608
MiliGuagu <57267175+MiliGuagu@users.noreply.github.com>
2021-11-21 13:37:06 -0600

    First product Schema

---
+++ server/models/products.js
@@ -0,0 +1,50 @@
+"use strict";
+
+/*
+// Conectarse a la instalcia local de MongoDB a traves de Mongoose
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+let MongoDB = 'mongodb://127.0.0.1:27017/Coffe4Home';
+let privateKey = process.env.TOKEN_KEY;
+
+let Utils = require('../controllers/utils');
+
+mongoose.connect(MongoDB, { useNewUrlParser: true });
+*/
+
+let productSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    image: {
+        type: String,
+    },
+    category: {
+        type: String,
+        required: true
+    },
+    stock: {
+        type: Number,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    updatedAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
