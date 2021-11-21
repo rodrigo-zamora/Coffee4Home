56344b390b4dd3d526981d26145b8d5d796ea617
MiliGuagu <57267175+MiliGuagu@users.noreply.github.com>
2021-11-21 13:31:33 -0600

    First order Schema

--- server/models/order.js
+++ server/models/order.js
@@ -6,3 +6,53 @@ const jwt = require('jsonwebtoken');
 let MongoDB = 'mongodb://localhost:27017/OrdersDB';
 let privateKey = process.env.TOKEN_KEY;
 */
+
+let orderSchema = new mongoose.Schema({
+    orderUUID: {
+        type: String,
+        required: false,
+        unique: true,
+        index: true,
+        default: Utils.generateOrderUUID()
+    },
+    userUUID: {
+        type: String,
+        required: true
+    },
+    orderDate: {
+        type: Date,
+        required: true
+    },
+    orderStatus: { 
+        type: String,
+        enum: ['PENDING', 'CONFIRMED', 'DELIVERED', 'CANCELLED'],
+        required: true
+    },
+    orderTotal: {
+        type: Number,
+        required: true
+    },
+    orderItems: [{
+        productId: {
+            type: String,
+            required: true
+        },
+        productName: {
+            type: String,
+            required: true
+        },
+        productPrice: {
+            type: Number,
+            required: true
+        },
+        productQuantity: {
+            type: Number,
+            required: true
+        },
+        productTotal: {
+            type: Number,
+            required: true
+        },
+    }],
+});
+
