4ee0b0aea5216570ee00fa8a6e212aad553e909d
Rodrigo Zamora <rodrigo_zamora_dav@hotmail.com>
2021-11-29 10:34:16 -0600

    Added working shoppingCart

--- app/public/js/cart.js
+++ app/public/js/cart.js
@@ -10,22 +10,19 @@ function productToHTML(product) {
     return `
     <div class="productContainer">
                     <div class="coffeeImg">
-                        <img class="productImg" src="resources/coffeeItems/peetsCoffee.jpg" alt="Peet's Coffee">
+                        <img class="productImg" src=${product.product.image} alt="Peet's Coffee">
                     </div>
                     <div class="coffeeIntormation">
-                        <h5 class="productTitle">Peet's Coffee</h5>
+                        <h5 class="productTitle">${product.product.name}</h5>
                         <div class="inputs">
                             <div class="input-quantity input-group mb-3">
                                 <span class="input-group-text">Cantidad:</span>
-                                <input type="number" class="quantityInput form-control" min="1" max="5" value="1"
+                                <input type="number" class="quantityInput form-control" min="1" max="5" value="${product.product.quantity}"
                                     id="quantity" disabled>
-                                <button class="pen btn btn-outline-secondary" type="button" for="quantity"
-                                    name="quantity" id="button-addon2"><i class="fa fa-pen"
-                                        aria-hidden="true"></i></button>
                             </div>
                             <div class="input-price input-group mb-3">
                                 <span class="input-group-text">Precio:</span>
-                                <input type="text" class="priceInput form-control " value="130.50">
+                                <input type="text" class="priceInput form-control " value="${product.product.pricePerUnit}">
                                 <span class="input-group-text">$ m.n.</span>
                             </div>
                             <button class="btn btn-outline-danger d-flex">Eliminar producto</button>
@@ -35,10 +32,33 @@ function productToHTML(product) {
     `
 }
 
-function addToCart(UUID) {
-    console.log(UUID);
-}
-
 function updatePage() {
-
+    let shoppingCart = JSON.parse(localStorage.getItem('shoppingCart'));
+    let products = [];
+    if (shoppingCart != []) {
+        // Make a petition to the server to get get each product for each item in the shopping cart
+        // and then add it to the page
+        for (let i = 0; i < shoppingCart.length; i++) {
+            // Make a request to /products + uuid
+            var xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function () {
+                if (this.readyState == 4 && this.status == 200) {
+                    // Convert the response to JSON
+                    var product = JSON.parse(this.responseText);
+                    // If the response is empty, show an error message
+                    product.product.quantity = shoppingCart[i].quantity;
+                    products.push(product);
+                }
+            }
+            xhttp.open("GET", "products/" + shoppingCart[i].UUID, false);
+            xhttp.send();
+        };
+        let productsHTML = '';
+        for (let i = 0; i < Object.keys(products).length; i++) {
+            productsHTML += productToHTML(products[i]);
+        }
+        cartContainer.innerHTML = productsHTML;
+    } else {
+        
+    }
 }
