8a937de37c365eb9afe17c6a2797bb75fdcd086e
Rodrigo Zamora <rodrigo_zamora_dav@hotmail.com>
2021-11-29 12:42:15 -0600

    Revert #76

--- server/controllers/router.js
+++ server/controllers/router.js
@@ -12,6 +12,7 @@ const tokenUtils = require('./utils/token_utils');
 router.route('/:email', tokenUtils.verifyToken);
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 //router.use('/orders', ordersRouter, " ? uuid = req.params.uuid ");
 router.use('/orders', ordersRouter);
 
@@ -26,5 +27,12 @@ router.use('/users', userRouter);
 router.use('/products', productRouter);
 >>>>>>> parent of 2ec8f91 (Patch login (#73))
 router.use('/admin', adminRouter);
+=======
+router.use('/orders', ordersRouter, " ? uuid = req.params.uuid ");
+//router.use('/users', userRouter);
+//router.use('/users', tokenUtils);
+router.use('/products', productRouter);
+router.use('/admin', adminRouter, " ? uuid = req.params.uuid ");
+>>>>>>> parent of 04324f9 (Changes for Login (#76))
 
 module.exports = router;
