ace3659620e71d7986938a10b6ea763c4c1038eb
Rodrigo Zamora <rodrigo_zamora_dav@hotmail.com>
2021-11-29 00:13:29 -0600

    Added searchProducts function

--- app/public/js/searchProducts.js
+++ app/public/js/searchProducts.js
@@ -3,12 +3,38 @@ searchButton.addEventListener("click", searchProducts);
 
 function searchProducts() {
     console.log("searchProducts");
-    
+    let tipoCafe = "";
+    let tipoGrano = "";
+    let cafeLocal = "";
 
-    let tipoGrano = document.getElementById("tipoGrano").value;
-    let cafeLocal = document.getElementById("cafeLocal").value;
+    var tipoCafeForm = document.getElementById("tipoCafe").elements;
+    for (i = 0; i < tipoCafeForm.length; i++) {
+        if (tipoCafeForm[i].checked) {
+            tipoCafe = tipoCafeForm[i].value;
+        }
+    }
+
+    var tipoGranoForm = document.getElementById("tipoGrano").elements;
+    for (i = 0; i < tipoGranoForm.length; i++) {
+        if (tipoGranoForm[i].checked) {
+            tipoGrano = tipoGranoForm[i].value;
+        }
+    }
+
+    var cafeLocalForm = document.getElementById("cafeLocal").elements;
+    for (i = 0; i < cafeLocalForm.length; i++) {
+        if (cafeLocalForm[i].checked) {
+            cafeLocal = cafeLocalForm[i].value;
+        }
+    }
+    
     let query = "?tipoCafe=" + tipoCafe + "&tipoGrano=" + tipoGrano + "&cafeLocal=" + cafeLocal;
     
     console.log(query);
 
+    // Send query to the server
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "/products" + query, true);
+    xhr.onreadystatechange = function () {
+    }
 }
