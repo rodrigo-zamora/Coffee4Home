6d15a93393ba3eca91937216265977c4e8bd28cf
Rodrigo Zamora <rodrigo_zamora_dav@hotmail.com>
2021-11-29 12:46:54 -0600

    Update router.js

--- server/controllers/router.js
+++ server/controllers/router.js
@@ -11,28 +11,9 @@ const tokenUtils = require('./utils/token_utils');
 
 router.route('/:email', tokenUtils.verifyToken);
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-//router.use('/orders', ordersRouter, " ? uuid = req.params.uuid ");
-router.use('/orders', ordersRouter);
-
-router.use('/users', userRouter);
-//router.use('/users', tokenUtils);
-router.use('/products', productRouter);
-
-//router.use('/admin', adminRouter, " ? uuid = req.params.uuid ");
-=======
 router.use('/orders', ordersRouter);
 router.use('/users', userRouter);
 router.use('/products', productRouter);
->>>>>>> parent of 2ec8f91 (Patch login (#73))
 router.use('/admin', adminRouter);
-=======
-router.use('/orders', ordersRouter, " ? uuid = req.params.uuid ");
-//router.use('/users', userRouter);
-//router.use('/users', tokenUtils);
-router.use('/products', productRouter);
-router.use('/admin', adminRouter, " ? uuid = req.params.uuid ");
->>>>>>> parent of 04324f9 (Changes for Login (#76))
 
 module.exports = router;
