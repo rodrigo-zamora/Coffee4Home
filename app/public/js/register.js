7efcb0203f90033fada3f6bc4f52d05cfb19917f
MiliGuagu <57267175+MiliGuagu@users.noreply.github.com>
2021-12-01 14:18:54 -0600

    Update register.js
    Working Sign Up

--- app/public/js/register.js
+++ app/public/js/register.js
@@ -34,58 +34,83 @@ zip.onchange = validatePassword;
 zip.onkeyup = validatePassword;
 
 function sendSignUp() {
-    console.log("sendSignUp");
-    var firstName_u = firstname.value;
-    var lastName_u = lastname.value;
-    var email_u = email.value;
-    var password_u = password1.value;
-    var phone_u = phone.value;
-    var street_u = street.value;
-    var city_u = city.value;
-    var state_u = state.value;
-    var zip_u = zip.value;
-    var newUser = {
-        "firstName": firstName_u,
-        "lastName": lastName_u,
-        "email": email_u,
-        "password": password_u,
-        "street": street_u,
-        "city": city_u,
-        "state": state_u,
-        "zip": zip_u,
-        "phone": phone_u,
-        "role": "USER"
-    };
-    var newUser2 = {
-        firstName: firstName_u,
-        lastName: lastName_u,
-        email: email_u,
-        password: password_u,
-        street: street_u,
-        city: city_u,
-        state: state_u,
-        zip: zip_u,
-        phone: phone_u,
-        role: "USER"
-    };
-    console.log(newUser);
-    console.log(newUser2);
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", "/admin/users", true);
-    xhr.setRequestHeader("Content-Type", "application/json");
-    xhr.setRequestHeader("x-auth", "admin");
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState === 4) {
-            if (xhr.status === 200) {
-                //window.location.href = "/?";
-                console.log(xhr.responseText);
-                console.log(newUser);
-                console.log(newUser2);
-            } else {
-                alert("Error: " + xhr.status);
+    if (firstname.value == "" || lastname.value == "" || email.value == "" || password1.value == "" || password2.value == "" || phone.value == "" || street.value == "" || city.value == "" || state.value == "" || zip.value == "") {
+        alert("Please fill out all fields");
+    }
+    else {
+        if(password1.value != password2.value) {
+            alert("Passwords don't match");
+        }
+        else if(zip.value.length != 5) {
+            alert("Zip code must be 5 digits");
+        }
+        else if (email.value.indexOf("@") == -1 || email.value.indexOf(".") == -1) {
+            alert("Please enter a valid email");
+        }
+        else if (phone.value.length != 10) {
+            alert("Please enter a valid phone number");
+        }
+        else if (isNaN(phone.value)) {
+            alert("Please enter a valid phone number");
+        }
+        else if (password1.value.length < 8) {
+            alert("Password must be at least 8 characters");
+        }
+        else if (String(password1.value).match(/(?=.*\d)(?=.*[a-z])(?=.*[A-Z])/) == false) {
+            alert("Password must contain at least one number, one lowercase and one uppercase letter");
+        }
+        else{
+            var newUser = {
+                firstName: firstname.value,
+                lastName: lastname.value,
+                email: email.value,
+                password: password1.value,
+                street: street.value,
+                city: city.value,
+                state: state.value,
+                zip: zip.value,
+                phone: phone.value,
+                role: "USER"
+            };
+            var xhr = new XMLHttpRequest();
+            xhr.open("POST", "/admin/users", true);
+            xhr.setRequestHeader("Content-Type", "application/json");
+            xhr.setRequestHeader("x-auth", "admin");
+            xhr.onreadystatechange = function() {
+                if (xhr.readyState === 4) {
+                    if (xhr.status === 200) {
+                        console.log(xhr.responseText);
+                        console.log(newUser);
+                        loginFromRegister(newUser.email, newUser.password);
+                    } else {
+                        alert("Error: " + xhr.status);
+                    }
+                }
             }
+            xhr.send(JSON.stringify(newUser));
         }
     }
-    //xhr.send(JSON.stringify(newUser));
+}
+
+function loginFromRegister(email, password) {
+    var data = {
+        email: email,
+        password: password
+    };
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "/login", true);
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            var response = xhr.responseText;
+            localStorage.setItem("token", response);
+            localStorage.setItem("email", email);
+            checkLogin();
+        }
+        else if (xhr.readyState === 4 && xhr.status !== 200) {
+            alert("Wrong email or password");
+        }
+    };
+    xhr.send(JSON.stringify(data));
 }
 
